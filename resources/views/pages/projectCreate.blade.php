0bc7ee70d432b4df6576760fc44323812f72d3f0
Daviduo <davide0400@gmail.com>
2023-02-13 10:39:27 +0100

    added style to create/edit project pages

--- resources/views/pages/projectCreate.blade.php
+++ resources/views/pages/projectCreate.blade.php
@@ -1,26 +1,54 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    
-    <h1>New task</h1>
-    <form method="POST" action="{{ route('project.store') }}" enctype="multipart/form-data">
-        @csrf
-        <label for="name">Name</label>
-        <input type="text" name="name">
-        <br>
-        <label for="main_image">Image</label>
-        <input type="file" name="main_image">
-        <br>
-        <label for="description">Description</label>
-        <textarea type="text" name="description" cols="40" rows="10"></textarea>
-        <br>
-        <label for="release_date">Release date</label>
-        <input type="date" name="release_date">
-        <br>
-        <label for="repo_link">Github repo link</label>
-        <input type="text" name="repo_link" value="http://github.com/">
-        <br>
-        <input type="submit" value="Crea nuovo progetto">
-    </form>
 
+<main>
+    <div class="main_title">
+        <h1>Nuovo progetto</h1>
+    </div>
+        <form method="POST" action="{{ route('project.store') }}" enctype="multipart/form-data">
+            @csrf
+
+            <div class="input_group">
+
+                <div class=" input_field">
+                    <label for="name"><i class="fa-solid fa-file-pen"></i> | Name</label><br>
+                    <input class="name_field field" type="text" name="name">
+                </div>
+                
+                <div class=" input_field">
+                    <label for="main_image"><i class="fa-solid fa-image"></i> | Image</label><br>
+                    <input class="image_field field" type="file" name="main_image">
+                </div>
+
+            </div>
+            
+            <div class="descr_field">
+                <label for="description"><i class="fa-solid fa-quote-left"></i> | Description</label><br>
+                <textarea class="field" type="text" name="description" cols="120" rows="10"></textarea>
+            </div>
+            
+            <div class="input_group">
+
+                <div class=" input_field">
+                    <label for="release_date"><i class="fa-solid fa-calendar-days"></i> | Release date</label><br>
+                    <input class="date_field field" type="date" name="release_date">
+                </div>
+                
+                <div class=" input_field">
+                    <label for="repo_link"><i class="fa-solid fa-link"></i> | Github repo link</label><br>
+                    <input class="link_field field" type="text" name="repo_link" value="http://github.com/">
+                </div>
+
+            </div>
+            
+            
+            <br>
+            <input class="card_principal_button" type="submit" value="Crea nuovo progetto">
+        </form>
+
+
+</main>
+        
+    
 @endsection
--- resources/views/pages/projectEdit.blade.php
+++ resources/views/pages/projectEdit.blade.php
@@ -2,27 +2,49 @@
 
 @section('content')
     
-    <h1>New task</h1>
-    <form method="POST" action="{{ route('project.update', $project) }}" enctype="multipart/form-data">
-        @csrf
-        <label for="name">Name</label>
-        <input type="text" name="name" value={{ $project -> name }}>
-        <br>
-        <label for="main_image">Image</label>
-        <input type="file" name="main_image"> <br>
-        Immagine Precedente: <br>
-        <img class="prj-image" src="{{ asset('storage/' . $project -> main_image) }}" alt=""> <br>
-        <br>
-        <label for="description">Description</label>
-        <textarea type="text" name="description" cols="40" rows="10" value={{ $project -> description }}></textarea>
-        <br>
-        <label for="release_date">Release date</label>
-        <input type="date" name="release_date" value={{ $project -> release_date }}>
-        <br>
-        <label for="repo_link">Github repo link</label>
-        <input type="text" name="repo_link" value={{ $project -> repo_link }}>
-        <br>
-        <input type="submit" value="Aggiorna progetto">
-    </form>
+<main>
+    <div class="main_title">
+        <h1>Modifica progetto</h1>
+    </div>
+        <form method="POST" action="{{ route('project.update', $project) }}" enctype="multipart/form-data">
+            @csrf
+
+            <div class="input_group">
+
+                <div class=" input_field">
+                    <label for="name"><i class="fa-solid fa-file-pen"></i> | Name</label><br>
+                    <input class="name_field field" type="text" name="name" value={{ $project -> name }}>
+                </div>
+
+                <div class=" input_field">
+                    <label for="main_image"><i class="fa-solid fa-image"></i> | Image</label><br>
+                    <input class="image_field field" type="file" name="main_image">
+                </div>
+
+            </div>
+
+            <div class=" input_field">
+                <label for="description"><i class="fa-solid fa-quote-left"></i> | Description</label><br>
+                <textarea class="field" type="text" name="description" cols="120" rows="10" value={{ $project -> description }}></textarea>
+            </div>
+
+            <div class="input_group">
+
+                <div class=" input_field">
+                    <label for="release_date"><i class="fa-solid fa-calendar-days"></i> | Release date</label><br>
+                    <input class="date_field field" type="date" name="release_date" value={{ $project -> release_date }}>
+                </div>
+
+                <div class=" input_field">
+                    <label for="repo_link"><i class="fa-solid fa-link"></i> | Github repo link</label><br>
+                    <input class="link_field field" type="text" name="repo_link" value={{ $project -> repo_link }}>
+                </div>
+
+            </div>
+
+            <input class="card_principal_button" type="submit" value="Aggiorna progetto">
+        </form>
+
+</main>
 
 @endsection
